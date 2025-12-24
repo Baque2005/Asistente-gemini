4fae6cb78d84f4ed2eb7c0ed1ee5ce437d7cd4bb
Baque2005 <155196295+Baque2005@users.noreply.github.com>
2025-12-23 21:58:30 -0500

    Primer commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Alexa = require('ask-sdk-core');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
